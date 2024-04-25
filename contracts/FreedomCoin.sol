8217304237fdb376fcbf78bdbbd07bcb433a102e
CyrusNejati <cyrusnejati@outlook.com>
2024-04-25 10:45:20 +0200

    add feature black list remove white list

--- contracts/FreedomCoin.sol
+++ contracts/FreedomCoin.sol
@@ -1,87 +1,44 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 
-contract FreedomCoin is ERC20, ERC20Permit, ERC20Votes, Ownable {
-    using EnumerableSet for EnumerableSet.AddressSet;
-    
-    EnumerableSet.AddressSet private _whitelist;
-    EnumerableSet.AddressSet private _blacklist;
+contract FreedomCoin is ERC20, Ownable, ERC20Permit {
+    mapping(address => bool) public blacklist;
+    address public charityAddress = 0xb93131eE7BE304c183790d3F6EdCDd978024F93f;
+    address public marketingDevAddress = 0xCA59eE89c37C78B29EC6Da733f66a2217042C7D3;
+    address public holderIncentive = 0xBF3C56f4D317A11e5796df613E52081A34462410;
 
     constructor(address initialOwner)
-        ERC20("FreedomCoin", "FREED")
-        ERC20Permit("FreedomCoin")
+        ERC20("Freedom Coin", "FREED")
         Ownable(initialOwner)
+        ERC20Permit("Freedom Coin")
     {
-        transferOwnership(initialOwner);
-        _mint(msg.sender, 7000000 * 10 ** decimals());
+        uint256 totalSupply = 7000000 * 10 ** decimals();
+        _mint(initialOwner, totalSupply);
+
+        uint256 charityAmount = totalSupply * 1 / 100; // 1%
+        uint256 incentiveAmount = totalSupply * 1 / 100; // 1%
+        uint256 marketingDevAmount = totalSupply * 0.9 / 100; // 0.9%
+
+        _transfer(initialOwner, charityAddress, charityAmount);
+        _transfer(initialOwner, holderIncentive, incentiveAmount);
+        _transfer(initialOwner, marketingDevAddress, marketingDevAmount);
     }
 
-    function clock() public view override returns (uint48) {
-        return uint48(block.timestamp);
+    function _transfer(address sender, address recipient, uint256 amount) internal override {
+        require(!blacklist[sender], "Sender is blacklisted");
+        require(!blacklist[recipient], "Recipient is blacklisted");
+        super._transfer(sender, recipient, amount);
     }
 
-    function CLOCK_MODE() public pure override returns (string memory) {
-        return "mode=timestamp";
+    function addToBlacklist(address _addr) public onlyOwner {
+        blacklist[_addr] = true;
     }
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20, ERC20Votes)
-    {
-        super._update(from, to, value);
-    }
-
-    function nonces(address owner)
-        public
-        view
-        override(ERC20Permit, ERC20Permit)
-        returns (uint256)
-    {
-        return super.nonces(owner);
-    }
-
-    // Whitelist and Blacklist functions
-
-    function addToWhitelist(address account) public onlyOwner {
-        require(!_whitelist.contains(account), "Account is already whitelisted");
-        _whitelist.add(account);
-    }
-
-    function removeFromWhitelist(address account) public onlyOwner {
-        require(_whitelist.contains(account), "Account not whitelisted");
-        _whitelist.remove(account);
-    }
-
-    function isWhitelisted(address account) public view returns (bool) {
-        return _whitelist.contains(account);
-    }
-
-    function addToBlacklist(address account) public onlyOwner {
-        require(!_blacklist.contains(account), "Account is already blacklisted");
-        _blacklist.add(account);
-    }
-
-    function removeFromBlacklist(address account) public onlyOwner {
-        require(_blacklist.contains(account), "Account not blacklisted");
-        _blacklist.remove(account);
-    }
-
-    function isBlacklisted(address account) public view returns (bool) {
-        return _blacklist.contains(account);
-    }
-
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        super._beforeTokenTransfer(from, to, amount);
-
-        require(!isBlacklisted(from), "Sender is blacklisted");
-        require(!isBlacklisted(to), "Recipient is blacklisted");
-        require(isWhitelisted(to) || to == address(0), "Recipient is not whitelisted");
+    function removeFromBlacklist(address _addr) public onlyOwner {
+        blacklist[_addr] = false;
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,36 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
+// require("@nomicfoundation/hardhat-toolbox")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("dotenv").config()
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-  solidity: "0.8.24",
+  etherscan: {
+    apiKey: "AHJFQN9UTSYFGA4BAYNDSG9QJ21W4X1WIT",
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: true
+  },
+  solidity: {
+    version: "0.8.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/6eaa3174aee0413ab852145e02fc306f",
+      accounts: ['0x63de17d346d140f9337d0e1ae524314aa27913f3ef526f7fb0c5a0afc31f374e']
+    },
+  },
 };
